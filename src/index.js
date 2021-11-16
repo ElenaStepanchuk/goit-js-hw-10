bbe6d4769c170b3f1ea7aacf764a4a30b44f8266
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-16 16:55:51 +0200

    Home-work 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetch-countries';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('input#search-box');
+const aboutCountry = document.querySelector('.country-info');
+
+console.log(input);
+console.log(aboutCountry);
+
+input.addEventListener('input', debounce(handleInputCountry, DEBOUNCE_DELAY));
+
+function handleInputCountry() {
+    const name = input.value.trim();
+    if (name === ``) {
+        return aboutCountry.innerHTML = ``;
+    }
+    fetchCountries(name).then(handleShowCountryList).catch(handleWrongCountryList)
+}
+
+function handleShowCountryList(array) {
+    console.log(array)
+    if (array.length > 10) {
+        aboutCountry.innerHTML = ``;
+    return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+    if (array.length >= 2) {    
+    return aboutCountry.innerHTML = handleCreateCountryList(array)
+    }
+    return aboutCountry.innerHTML = handleCreateAboutCountryList(array);
+}
+
+function handleWrongCountryList(error) {
+    console.log(error)
+    return aboutCountry.innerHTML = ``
+}
+
+function handleCreateCountryList(array) {
+    return array.map(({flags, name}) => {
+        return `<p class="name-country">
+    <img src="${flags.svg}" alt="" width="20px" height="20px">
+      ${name.official}
+      </p>`
+    }).join("")
+}
+
+
+function handleCreateAboutCountryList(array) {
+  return array.map(({ flags, name, capital, population, languages }) => {
+        return `<h1 class="name-country">
+    <img src="${flags.svg}" alt="" width="20px" height="20px">
+      ${name.official}
+      </h1>
+      <ul class="list">
+      ${capital ? `<li class="country-info">
+          <span>Capital:</span> ${capital}
+        </li>` : ``}
+              ${population ? `<li class="country-info">
+          <span>Population:</span> ${population}
+        </li>` : ``}
+              ${Object.values(languages) ? `<li class="country-info">
+          <span>Languages:</span> ${Object.values(languages)}
+        </li>` : ``}
+      </ul>`
+    }).join("")
+}
